512f9cee15a0fc629c92166c7f4ad97e663dac37
denis.rodriguez <denis.rodriguez@et.bo>
2022-10-03 14:35:48 -0400

    FINISH
    End project tutorial

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { } from 'react'
+import { StatusBar, Text, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './component/screen/Home'
+import MyCart from './component/screen/MyCart'
+import ProductInfo from './component/screen/ProductInfo'
 
 const App = () => {
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View>
-      <Text> HELLO</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='MyCart' component={MyCart} />
+        <Stack.Screen name='ProductInfo' component={ProductInfo} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
